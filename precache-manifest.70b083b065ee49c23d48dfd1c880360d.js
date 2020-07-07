ce82d95a0dcceffb718f3ccfe6a7f75541913c3c
radhikakevat <radhikakevat1995@gmail.com>
2020-07-07 15:35:56 +0530

    Updates

---
+++ precache-manifest.70b083b065ee49c23d48dfd1c880360d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7de352284f1632bcd180d6521241ee5",
+    "url": "/todoList-app/index.html"
+  },
+  {
+    "revision": "0a44c2977352b76d2d7e",
+    "url": "/todoList-app/static/css/main.2a39a444.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/todoList-app/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoList-app/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a44c2977352b76d2d7e",
+    "url": "/todoList-app/static/js/main.9ed8f925.chunk.js"
+  },
+  {
+    "revision": "dcff2e67a0d7c6284a30",
+    "url": "/todoList-app/static/js/runtime-main.19db8e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoList-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
